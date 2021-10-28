7712e9345c03d8dcd29039460dfa70b860e8462d
HyperHydroX <guillaumedebackere7@gmail.com>
2021-10-28 14:57:11 +0200

    robot aniamtion done

--- script/app.js
+++ script/app.js
@@ -18,26 +18,39 @@
 //   duration: .75,
 //   scale: 0.75,
 // })
-let duration_time = 1;
-const fast = document.querySelector(".js-fast");
-const normal = document.querySelector(".js-normal");
-const slow = document
 
-// normal.addEventListener("clicked" = () => {
+// LISTERNERS
+const btn_fast = document.querySelector(".js-fast");
+const btn_normal = document.querySelector(".js-normal");
+const btn_slow = document.querySelector(".js-slow");
+const btn_controls = document.querySelector(".js-pause");
+const btn_play = btn_controls.querySelector(".c-play-icon--play")
+const btn_pause = btn_controls.querySelector(".c-play-icon--pause")
 
-// });
-.querySelector(".js-slow");
 
+// EVENTS
+btn_fast.onclick = () => tl.timeScale(2);
+btn_normal.onclick = () => tl.timeScale(1);
+btn_slow.onclick = () => tl.timeScale(.5);
+btn_pause.onclick = () => tl.pause();
+btn_play.onclick = () => tl.resume();
+
+// ANIMATION
 let tl = gsap.timeline({
   defaults: {
-    duration: duration_time,
+    duration: 1,
     ease: 'power1.inOut',
   },
   repeat: -1,
+  yoyoEase: 'Power2.easeOut',
   yoyo: true,
 })
 
 tl.set('#shadow',{
+    transformOrigin: "50% 100%" 
+});
+
+tl.set('#Fire',{
     transformOrigin: "50% 100%" 
 });
 
@@ -50,4 +63,25 @@ tl.fromTo('#robot',{
   scale: 0.75,
 },
   '<',
-)
+);
+
+tl.to('#Fire', {
+  scale: 1.25,
+  y: 3,
+}, 0);
+
+tl.to('#Torso', {
+  y: -1,
+}, 0);
+
+tl.to('#Arm-left', {
+  rotation: -2.5,
+}, 0);
+
+tl.to('#Arm-right', {
+  rotation: 2.5,
+}, 0);
+
+
+
+//
