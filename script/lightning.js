268915437fdb0be41e11227885cf217ecb5e78bd
HyperHydroX <guillaumedebackere7@gmail.com>
2021-11-02 11:11:18 +0100

    done

--- script/lightning.js
+++ script/lightning.js
@@ -1,6 +1,7 @@
 // LIGHTNING
 const btn_lightning = document.querySelector(".js-lightning");
 const animation = document.querySelector("#lightning");
+
 btn_lightning.onclick = () => {
     animation.beginElement();
 }
